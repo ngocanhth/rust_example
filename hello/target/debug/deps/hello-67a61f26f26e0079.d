b4d62e406b14f27b566e4b7eaab2e461ef56a2a8
Ngoc Anh <anhln2@arrowhitech.net>
2022-03-27 21:31:41 +0700

    push source  code to github

---
+++ hello/target/debug/deps/hello-67a61f26f26e0079.d
@@ -0,0 +1,6 @@
+/home/aht/Projects/Rust/rust_example/hello/target/debug/deps/hello-67a61f26f26e0079: src/main.rs src/formatted_print.rs
+
+/home/aht/Projects/Rust/rust_example/hello/target/debug/deps/hello-67a61f26f26e0079.d: src/main.rs src/formatted_print.rs
+
+src/main.rs:
+src/formatted_print.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/home/aht/Projects/Rust/rust_example/hello/target/debug/hello: /home/aht/Projects/Rust/rust_example/hello/src/formatted_print.rs /home/aht/Projects/Rust/rust_example/hello/src/main.rs
